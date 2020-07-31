23ac9531c1e1c36b3b36a2a75a37dc0b38976253
connorworthen <connorw220@protonmail.com>
2020-07-30 17:59:33 -0700

    main root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+
+  def login
+    if logged_in?
+      redirect_to user_path(session[:user_id])
+    else 
+      render 'login'
+    end 
+  end 
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      redirect_to '/login'
+    end 
+  end 
+    
+  def destroy 
+    session.delete :user_id 
+    redirect_to '/'
+  end 
+
+  private 
+
+    def user_paramas
+      params.require(:user).permit(:username, :password)
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'main/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'main#home'
+  get '/signup', to: 'users#new', as: "new_user"
+  post '/signup', to: 'users#create', as: 'users_path'
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
