fcfce1f7674bcc24d27c21c2ef3821abc4656044
connorworthen <connorw220@protonmail.com>
2020-08-02 14:03:38 -0700

    edit update destroy methods

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,2 +1,12 @@
 class ManufacturersController < ApplicationController
+
+    def index 
+        @manufacturers = Manufacturer.search(params[:search])
+    end
+
+    def show
+        @manufacturer = Manufacturer.find_by(id: params[:id])
+        @products = Product.alphabetize(@manufacturer.id)
+    end 
+    
 end
