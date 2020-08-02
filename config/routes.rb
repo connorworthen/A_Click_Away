2ac3a1f554e22b9939187f4812adb3640549b69e
connorworthen <connorw220@protonmail.com>
2020-08-02 13:55:47 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,31 @@
 Rails.application.routes.draw do
-  get 'main/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root to: 'main#home'
+ 
+  resources :manufacturers, only: [:index, :search, :show] do 
+    resources :products, only: [:new, :create]
+  end 
+  resources :products, only: [:new, :create, :show, :edit, :update] do 
+    resources :reviews, only: [:new, :create, :show, :edit, :update]
+  end 
+  resources :users, only: [:new, :create, :show]
+
+  get '/' to 'main#home'
   get '/signup', to: 'users#new', as: "new_user"
   post '/signup', to: 'users#create', as: 'users_path'
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  post '/products/new', to: 'products#create'
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  post '/products/:id/reviews/new', to: 'reviews#create'
+  post '/users/:user_id/create_follow', to: 'follows#create', as: 'create_follow'
+  post '/users/:user_id/unfollow', to: 'follows#unfollow', as: 'unfollow'
 
-  resources :users, only: [:new, :create, :show]
+  post '/reviews/:id', to: 'reviews#destroy', as: 'delete_review'
+
+  get '/users/:user_id/following', to: 'follows#following', as: 'user_following'
+  get '/users/:user_id/followers', to:  'follows#followers', as: 'user_follower'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
