5e5ba1684d5ee0c392810f8a8ad9b08a7c4cd7c6
connorworthen <connorw220@protonmail.com>
2020-08-16 14:01:13 -0700

    nested route reviews + products

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  get 'users/:id/products' => 'products#index', :as => :user_products_path
+  get 'users/:id/manufacturers' => 'manufacturers#index', :as => :user_manufacturers_path
 
   resource :products
-  # resource :product
   resource :manufacturers
+  resource :reviews
+  resource :users
 
   resources :manufacturers, only: [:show, :search, :new, :create, :update, :destroy] do
     resources :products, only: [:new, :create]
@@ -25,24 +28,9 @@ Rails.application.routes.draw do
     resources :manufacturers
   end
 
-  get 'users/:id/products' => 'products#index', :as => :user_products_path
-  
-  get 'users/:id/manufacturers' => 'manufacturers#index', :as => :user_manufacturers_path
-  # get 'products', action: :index, controller: 'index'
-
-  # resources :users, only: [:new, :create, :show]
-
-  # resources :products, only: [:new, :create, :show, :edit, :update] do
-  # #   resources :reviews, only: [:new, :create, :show, :edit, :update]
-  # # end
-
-  # get '/manufacturers', to: 'manufacturers#show'
-  # get '/auth/:provider/callback', to: 'sessions#omniauth'
-
-  # get '/products/:id', to: 'products#edit'
-  # get '/products', to: 'products#show'
-  # post '/products/new', to: 'products#create'
-
+  resources :products, only: [:new, :create, :show, :edit, :update] do 
+    resources :reviews, only: [:new, :create, :show, :edit, :update]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
