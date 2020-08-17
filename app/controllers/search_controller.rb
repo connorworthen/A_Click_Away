1f8564a4e49da9d9a12ba2f1dff770e55d88c26f
connorworthen <connorw220@protonmail.com>
2020-08-17 10:13:21 -0700

    now able to search products

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,14 @@
 class SearchController < ApplicationController
-  def search
+
+  def search  
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else
+      @parameter = params[:search].downcase 
+      @product = Product.find_by(params[:id])
+      @products = Product.all 
+      @product = Product.all.where("lower(name) LIKE :search", search: "%#(@parameter)#%")  
+    end  
   end
+
 end
