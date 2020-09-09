6b0bb7c4115e680cdc8551ed65b34f2aa9d4c133
connorworthen <connorw220@protonmail.com>
2020-09-08 20:53:18 -0700

    deleted unused method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,19 +18,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # def create
-  #   if request.env['omniauth.auth']
-  #     user = User.create_with_omniauth(request.env['omniauth.auth'])
-  #     session[:user_id] = user.id    
-  #     redirect_to user_path(user.id)
-  #   else
-  #     user = User.find_by_email(params[:email])
-  #     user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
-  #     redirect_to user_path(user.id)
-  #   end
-  # end
-    
   def destroy 
     session.delete :user_id 
     redirect_to root_path
