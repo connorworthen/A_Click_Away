92d2808752905f8c3c7fef94ef9d07cb4e49fc2f
connorworthen <connorw220@protonmail.com>
2020-09-10 09:46:13 -0700

    facebook login/parsing fails

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'pry'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'omniauth-facebook'
 gem 'dotenv-rails'
-gem 'thin'
 # gem 'figaro'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.credentials.facebook(:app_id), Rails.application.credentials.facebook(:secret)
+  provider :facebook, Rails.application.credentials.facebook(:app_id), Rails.application.credentials.facebook(:app_secret)
 end
